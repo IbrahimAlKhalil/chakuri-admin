fd8a6d2445df59ad058d6f850646188ebe58b847
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2020-04-12 07:20:00 +0600

    fix: add positions reorder functionality

--- src/pages/dashboard/pages/positions/page.vue
+++ src/pages/dashboard/pages/positions/page.vue
@@ -6,11 +6,24 @@
                :edit-form="editForm"
                :per-page="25">
 
+        <template #tool-btns="{data}">
+            <el-tooltip v-if="reordered" content="Save Current Order">
+                <el-button icon="fa fa-save" @click="updateOrder(data.items)" circle></el-button>
+            </el-tooltip>
+        </template>
+
+        <template #tool-menu="{data}">
+            <div v-if="reordered" class="el-menu-item" @click="updateOrder(data.items)"><i
+                    class="fa fa-save"></i> Save
+                Order
+            </div>
+        </template>
+
         <template #default="{data, methods}">
 
             <items-count :data="data"/>
 
-            <data-table :cols="cols" :data="data" :methods="methods">
+            <data-table :cols="cols" :data="data" :methods="methods" @dragged="reordered=true" draggable>
                 <template #default="{item}">
                     <td v-highlight="data.highlight">{{item.name}}</td>
                 </template>
@@ -23,9 +36,10 @@
     import dataList from '@/components/data-list/lazy';
     import dataTable from '@/components/table';
     import itemsCount from '@/components/items-count';
+    import {elButton, elTooltip} from '@/el';
 
     export default {
-        components: {itemsCount, dataList, dataTable},
+        components: {itemsCount, dataList, dataTable, elButton, elTooltip},
 
         data() {
             const required = {required: true};
@@ -44,7 +58,8 @@
 
                 editForm: form,
 
-                cols: ['Name']
+                cols: ['Name'],
+                reordered: false
             };
         },
 
@@ -54,6 +69,30 @@
 
                 return item;
             },
+
+            async updateOrder(items) {
+                const orders = items.map((item, order) => {
+                    return {
+                        id: item.id,
+                        order: order + 1
+                    };
+                });
+
+                this.$fetch('dashboard/positions/reorder', {
+                    method: 'POST',
+                    body: {
+                        orders
+                    }
+                }).response();
+
+                this.$notify({
+                    type: 'success',
+                    title: 'Success',
+                    message: 'Updated'
+                });
+
+                this.unsavedOrder = false;
+            }
         }
     };
 </script>
